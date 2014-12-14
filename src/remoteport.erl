35f864063336e57cf31554cb6a261539d800baf3
virtan <virtan@virtan.com>
2014-12-13 16:58:32 -0800

    remoteport debug

--- src/remoteport.erl
+++ src/remoteport.erl
@@ -9,7 +9,7 @@
 % returns pid of remote process
 start(Node, PortName, PortSettings) ->
     Caller = self(),
-    spawn(Node, ?MODULE, port_loop, [Caller, PortName, PortSettings]).
+    spawn_link(Node, ?MODULE, port_loop, [Caller, PortName, PortSettings]).
 
 stop(Pid) ->
     Pid ! {self(), close}.
@@ -21,18 +21,22 @@ port_loop(Caller, PortName, PortSettings) ->
 port_loop_2(Caller, Port) ->
     receive
         {Caller, {command, Data}} ->
+            io:format("sending to port~n", []),
             Port ! {self(), {command, Data}},
             port_loop_2(Caller, Port);
         {Caller, close} ->
+            io:format("closing port~n", []),
             Port ! {self(), close},
             port_loop_2(Caller, Port);
         {Caller, {connect, NewCaller}} ->
             Caller ! {self(), connected},
             port_loop_2(NewCaller, Port);
         {Port, {data, Data}} ->
+            io:format("data from port~n", []),
             Caller ! {self(), {data, Data}},
             port_loop_2(Caller, Port);
         {Port, closed} ->
+            io:format("port closed~n", []),
             Caller ! {self(), closed},
             ok;
         {Port, {exit_status, Status}} ->
